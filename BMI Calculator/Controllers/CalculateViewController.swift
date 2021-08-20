29a7d09656a75633e15d2b012b19c222445fdaa1
Nikki Personal <nikkipersonal@NRae-PL.local>
2021-08-20 11:31:43 +0100

    First commit

---
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+
+    @IBOutlet weak var heightValueLabel: UILabel!
+    @IBOutlet weak var weightValueLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    var calculatorBrain = CalculatorBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let value = String(format: "%.2f", sender.value)
+        heightValueLabel.text = "\(value)m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let value = String(format: "%.1f", sender.value)
+        weightValueLabel.text = "\(value)kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        calculatorBrain.calculateBMI(height: height, weight: weight)
+        
+        self.performSegue(withIdentifier: "goToResults", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        if segue.identifier == "goToResults" {
+            let destinationVC = segue.destination as! ResultsViewController
+            destinationVC.bmiValue = calculatorBrain.getBMIValue()
+            destinationVC.advice = calculatorBrain.getAdvice()
+            destinationVC.color = calculatorBrain.getColor()
+        }
+    }
+    
+}
+
---
+++ README.md
@@ -0,0 +1,8 @@
+## What you will learn
+
+* How to create multi-screen apps with animated navigation.
+* Optional binding, optional chaining and the nil coalescing operator.
+* How to create classes and difference between classes and structs. 
+* Pass by value vs. pass by reference. 
+* Formatting Strings. 
+* Color literals.
